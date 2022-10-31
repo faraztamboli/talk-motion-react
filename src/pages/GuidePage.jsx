e5a4ad0701aae870f0191ffa40a66c774dcd2ed3
unknown <muhammadtayyab3411@gmail.com>
2022-10-31 16:44:16 +0500

    Header added on Guide Page

--- src/pages/GuidePage.jsx
+++ src/pages/GuidePage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import GuidePageContent from '../components/ui/GuidePageContent';
-import GuidePageNavButtons from '../components/ui/GuidePageNavButtons';
 import GuidePageVideo from '../components/ui/GuidePageVideo';
 import { Col, Row } from 'antd';
+import Header from '../components/Layout/home/Header';
 
 function GuidePage(props) {
   return (
     <>
-      <div className="guide-page-container" style={{ backgroundColor: 'rgb(249 249 249)' }}>
-        <Row style={{ minHeight: '100vh' }} className="guide-page">
-          <Col span={24}>
-            <GuidePageNavButtons sm={props.sm} />
-          </Col>
+      <Header md={props.md} />
+      <div
+        className="guide-page-container"
+        style={{ backgroundColor: 'rgb(249 249 249)', paddingTop: '3rem' }}
+      >
+        <Row style={{ minHeight: '83vh' }} className="guide-page">
           <Col span={12} xs={24} lg={12}>
             <GuidePageVideo lg={props.lg} />
           </Col>
