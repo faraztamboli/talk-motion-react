7c4fd6292ead2b28256253db7f4b01c3243fa159
Neelofar Tamboli <tamboli.neelofar@gmail.com>
2023-01-12 16:13:20 -0500

    added photo for 3m in stories

--- Landing Page/src/components/StoriesSection.jsx
+++ Landing Page/src/components/StoriesSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import coupleImage from "../media/images/couple-talking-using-sign-language 1.png";
-import unsplash_1 from "../media/images/unsplash_PddcoUGPBMw.png";
-import unsplash_2 from "../media/images/unsplash_ylqvfBv8OpQ.png";
+import unsplash_1 from "..//images/unmediasplash_PddcoUGPBMw.png";
+import unsplash_2 from "C:\Users\tambo\Downloads\Faraz-Headshot.jpg";
 
 import { Col, Row } from "antd";
 import { Link } from "react-router-dom";
