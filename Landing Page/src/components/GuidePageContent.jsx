5c5a9178e1f7771514f75c817df6aa26eb196c10
Faraz Farukh Tamboli <faraz.tamboli@gmail.com>
2023-05-26 15:15:44 -0400

    Wording change from gesture to sign language

--- Landing Page/src/components/GuidePageContent.jsx
+++ Landing Page/src/components/GuidePageContent.jsx
@@ -15,23 +15,23 @@ function GuidePageContent(props) {
         </div>
         <div className="content" style={contentStyle}>
           <h2 className="guide-page-content-h2">
-            Train the system to earn points
+            Sign language gestures to voice
           </h2>
           <p className="guide-page-content-p w-100p">
-            TalkMotion uses the power of AI to translate sign language to voice
-            and voice to sign language. It allows you to create and train your
-            own custom gestures. We are working with experts in ASL to map a
-            wide range of gestures.
+            Deaf and aphonic people can sign and their movements will be picked
+            up by their webcam. An Artificial Intelligence algorithm
+            will convert these gestures into voice.
           </p>
         </div>
       </div>
+
       <div className="con con-1">
         <div className="number">
           <p className="guide-page-number-circle">2</p>
         </div>
         <div className="content" style={contentStyle}>
           <h2 className="guide-page-content-h2">
-            Convert your voice to gestures
+            Voice to sign language gestures
           </h2>
           <p className="guide-page-content-p w-100p">
             A hearing person can verbally respond, and TalkMotion will display
@@ -41,18 +41,52 @@ function GuidePageContent(props) {
           </p>
         </div>
       </div>
+
       <div className="con con-1">
         <div className="number">
           <p className="guide-page-number-circle">3</p>
         </div>
         <div className="content" style={contentStyle}>
           <h2 className="guide-page-content-h2">
-            Convert your gestures to voice
+            Professional models for various settings
           </h2>
           <p className="guide-page-content-p w-100p">
-            Deaf and aphonic people can sign and their movements will be picked
-            up by the gesture detector. An Artificial Intelligence algorithm
-            will convert these gestures into voice.
+            TalkMotion offers translation models for a wide range of applications.
+            Whether you want to communicate in retail shops, healthcare settings,
+            at workplace or talking to your loved ones, we have you covered!
+            We are working with experts in ASL to map a wide range of gestures.
+          </p>
+        </div>
+      </div>
+
+      <div className="con con-1">
+        <div className="number">
+          <p className="guide-page-number-circle">4</p>
+        </div>
+        <div className="content" style={contentStyle}>
+          <h2 className="guide-page-content-h2">
+            Personalized models
+          </h2>
+          <p className="guide-page-content-p w-100p">
+            TalkMotion uses the power of Artificial Intelligence to translate sign language to voice
+            and voice to sign language. It allows you to create and train your own custom gestures.
+            We are working with experts in ASL to map a wide range of gestures.
+          </p>
+        </div>
+      </div>
+
+      <div className="con con-1">
+        <div className="number">
+          <p className="guide-page-number-circle">5</p>
+        </div>
+        <div className="content" style={contentStyle}>
+          <h2 className="guide-page-content-h2">
+            TalkMotion in Education
+          </h2>
+          <p className="guide-page-content-p w-100p">
+            TalkMotion makes educational video content available to the deaf and aphonic community
+            in their own native communication platform sign language. Students can learn the same
+            as their hearing peers.
           </p>
         </div>
       </div>
