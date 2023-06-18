0bea5b857971953306e7c0de1253cd8b1dacdcd5
Bilal Ahmad Afridi <bilalkhan891@hotmail.com>
2023-06-18 23:18:15 +0500

    My Subscription page added

---
+++ src/pages/MySubscriptions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MySubscriptions = () => {
+  return (
+    <div>
+      <h1>MySubscriptions</h1>
+    </div>
+  );
+};
+
+MySubscriptions.propTypes = {};
+
+export default MySubscriptions;
