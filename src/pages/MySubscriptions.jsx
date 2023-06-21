92729bea2c3b76379ceabfe0f7f9f17b8152fee5
Bilal Ahmad Afridi <bilalkhan891@hotmail.com>
2023-06-21 23:15:00 +0500

    Top card templated added on My Subscriptions page

--- src/pages/MySubscriptions.jsx
+++ src/pages/MySubscriptions.jsx
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Descriptions } from "antd";
+import {
+  Card,
+  Row,
+  Col,
+  Space,
+  Avatar,
+  Button,
+  List,
+  Skeleton,
+} from "antd";
 import UpdateProfile from "../components/ui/UpdateProfile";
 import UserMenuProfileItem from "../components/ui/UserMenuProfileItem";
 import useMySubscriptions from "../hooks/useMySubscriptions";
 
 function MySubscriptions(props) {
+  const [initLoading, setInitLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
   const [purchaseList, setPurchaseList] = useState();
   const { getPurchaseList, getPurchaseDetail } = useMySubscriptions();
 
@@ -26,23 +39,84 @@ function MySubscriptions(props) {
   return (
     <>
       <div style={style} className="layout-bg mh-100vh">
-        <div>
-          <UserMenuProfileItem size="large" />
-        </div>
         <div
           className="details_section"
           style={{ marginTop: "2rem" }}
         >
-          <Descriptions
-            layout="horizontal"
-            column={1}
-            bordered
-            labelStyle={{ backgroundColor: "whitesmoke" }}
-          >
-            <Descriptions.Item label="Username">
-              {"xyz"}
-            </Descriptions.Item>
-          </Descriptions>
+          <Row gutter={16}>
+            <Col span={6}>
+              <Card title="Recuring Charges" bordered={false}>
+                <Space
+                  size="middle"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  Module <strong>123.12</strong>
+                </Space>
+                <Space
+                  size="middle"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  Module <strong>123.12</strong>
+                </Space>
+                <Space
+                  size="middle"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  Module <strong>123.12</strong>
+                </Space>
+
+                <hr />
+                <Space
+                  size="middle"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <strong>Total:</strong>
+                  <strong>345</strong>
+                </Space>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+          <Row gutter={16}>
+            <Col span={8}>
+              <List
+                className="demo-loadmore-list"
+                itemLayout="horizontal"
+                renderItem={(item) => (
+                  <List.Item
+                    actions={[
+                      <a key="list-loadmore-edit">edit</a>,
+                      <a key="list-loadmore-more">more</a>,
+                    ]}
+                  >
+                    <Skeleton>
+                      <List.Item.Meta
+                        title={
+                          <a href="https://ant.design">
+                            {item.name?.last}
+                          </a>
+                        }
+                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                      />
+                      <div>content</div>
+                    </Skeleton>
+                  </List.Item>
+                )}
+              />
+            </Col>
+          </Row>
         </div>
       </div>
     </>
